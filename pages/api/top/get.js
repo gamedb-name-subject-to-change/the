d87bbd42d4d63697776359b0c1922021fceb462a
kskarthik5 <kskarthik@gmail.com>
2022-02-09 15:47:10 +0530

    beta6

--- pages/api/top/get.js
+++ pages/api/top/get.js
@@ -1,7 +1,6 @@
 
 import axios from 'axios';
 export default async function handler(req, res) {
-    const url = `https://steamspy.com/api.php?request=top100in2weeks`
-    const result = await axios.get(url).then(async (res) => await res.data)
+    const result = require('../../../topgames.json')
     res.json({data:result})
 }
