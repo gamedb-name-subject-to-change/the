c22e96d98eaa014bb1434de6b23385159b0a8219
Tejas Nair <tejasnair2002@gmail.com>
2022-02-09 12:54:38 +0530

    recommended games added

---
+++ pages/api/top/get.js
@@ -0,0 +1,7 @@
+import mongoose from 'mongoose';
+import axios from 'axios';
+export default async function handler(req, res) {
+    const url = `https://steamspy.com/api.php?request=top100in2weeks`
+    const result = await axios.get(url).then(async (res) => await res.data)
+    res.json({data:result})
+}
