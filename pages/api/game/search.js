f5c2928330a4b30bd9920a5f6fab9ffb05fd9306
Your Name <kskarthik20025@gmail.com>
2022-02-11 22:22:58 +0530

    second

--- pages/api/game/search.js
+++ pages/api/game/search.js
@@ -34,17 +34,14 @@ export default async function handler(req, res) {
         matches = await  GameDB.find( { name: regex } )
     }
     console.log(text, matches.length)
-    let count=0;
-    for(let i in matches){
-        if(i<index)i=index;
-        let url = `https://store.steampowered.com/api/appdetails?appids=${matches[i].appid}`
-        let res = await axios.get(url).then(async (res) => await res.data).catch(e=>{console.log(e)})
+    let Q=matches.slice(index,index+6)
+    index=index+6;
+    for(let e of Q){
+        let url = `https://store.steampowered.com/api/appdetails?appids=${e.appid}`
+        let res = await axios.get(url).then(async (res) => await res.data)
         let temp=Object.values(res)[0].data
         results.push(temp);
-        if(++count==6)break
-    }
-    index=index+6;
-    console.log(results)   
+    }   
     res.json({ data: results, more: (matches[index])?true:false})
     return;
 }
