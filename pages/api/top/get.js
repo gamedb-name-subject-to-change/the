bf52e49c121d14c2ae95b1e2ca3d9d22f0571637
Your Name <kskarthik20025@gmail.com>
2022-02-12 00:02:02 +0530

    second

--- pages/api/top/get.js
+++ pages/api/top/get.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
     let randint = Math.floor(Math.random() * 90);
     let select = data.slice(randint, randint + 10)
     for (const e of select) {
-        const url = `https://store.steampowered.com/api/appdetails?appids=${appID}`
+        const url = `https://store.steampowered.com/api/appdetails?appids=${e.appid}`
         const res = await axios.get(url).then(async (res) => await res.data)
         let temp = (res.data)?Object.values(res.data)[0]:undefined
         if(temp) if(temp.success==true)results.push(temp.data)
