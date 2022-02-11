7aa78f6264e3d0ff850d472dcf97786b8208143b
Your Name <kskarthik20025@gmail.com>
2022-02-11 23:43:26 +0530

    second

--- pages/api/top/get.js
+++ pages/api/top/get.js
@@ -4,12 +4,14 @@ export default async function handler(req, res) {
     const appIDs = require('../../../topgames.json')
     let results = []
     let data = Object.values(appIDs)
+    console.log(data.length)
     let randint = getRandomInt(90)
     for (const e of data.slice(randint, randint + 10)) {
         const res = await fetchGameData(e.appid)
         let temp = Object.values(res.data)[0]
+        console.log(temp)
         if (temp.success == true && temp.data.type === 'game')
-            results.push(temp)
+            results.push(temp.data)
     }
     res.json({data:results})
 }
