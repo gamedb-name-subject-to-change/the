0e1a0f6dd937144563dfae0388ff982ef56b3391
Your Name <kskarthik20025@gmail.com>
2022-02-12 23:31:12 +0530

    game forum activity component

--- components/relatedposts.js
+++ components/relatedposts.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
+import axios from 'axios';
 const RelatedPosts = ({props}) => {
     const [posts, setPosts] = useState(null)
     const renderPosts = (data) => {
